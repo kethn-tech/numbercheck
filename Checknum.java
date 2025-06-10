963cfb9337ec1adf4c7f27bbfeacceec4ea9e4e7
Kethan Ayatti <kethanayatti333@gmail.com>
2025-06-10 19:03:59 +0530

    first commit

---
+++ Checknum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Checknum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } 
+    }
+}
